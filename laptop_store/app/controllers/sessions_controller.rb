abd5a9c5459053875bdf707063558936f1875163
05sahil <sahil.jha@berylsystems.com>
2020-03-04 19:42:58 +0530

    update create function for authentication

--- laptop_store/app/controllers/sessions_controller.rb
+++ laptop_store/app/controllers/sessions_controller.rb
@@ -2,6 +2,13 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create
+def create
+    @user = User.find_by(username: params[:username])
+    if (@user and @user.username == params[:username] and @user.password == params[:password])
+    	session[:user_id] = @user.id
+    	redirect_to users_path
+    else 
+    	render :new
+    end
   end
 end
