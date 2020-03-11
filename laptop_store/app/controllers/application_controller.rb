79afb0334d224c70caf3ef9d38fca06c897287db
05sahil <sahil.jha@berylsystems.com>
2020-03-11 17:15:21 +0530

    add helper method find_products

--- laptop_store/app/controllers/application_controller.rb
+++ laptop_store/app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
   helper_method :logged_in?
+  helper_method :find_products
 
-  
   def current_user
   	User.find_by(id: session[:user_id])
   end
@@ -11,4 +11,13 @@ class ApplicationController < ActionController::Base
     !current_user.nil?
   end
 
+  def find_products(user_id)
+  	@products_info = User.find(user_id).orders.find_by("payment_status != 'received'").order_products
+ 	@products = Array.new(@products_info.length){Hash.new}  #@product_info.length
+ 	@products_info.each{|p| 
+ 	  @products[@products_info.index(p)]['name'] = Laptop.find(p.laptop_id).name
+ 	  @products[@products_info.index(p)]['price'] = Laptop.find(p.laptop_id).price
+ 	  @products[@products_info.index(p)]['quantity'] = p.quantity 
+ 	}   	
+  end
 end
--- laptop_store/app/controllers/carts_controller.rb
+++ laptop_store/app/controllers/carts_controller.rb
@@ -1,14 +1,8 @@
 class CartsController < ApplicationController
   def show
-    if session[:user_id]!=''	
-      @user_id = session[:user_id]
-  	  @products_info = User.find(@user_id).orders.find_by("payment_status != 'received'").order_products
- 	  @products = Array.new(@products_info.length){Hash.new}  #@product_info.length
- 	  @products_info.each{|p| 
- 	    @products[@products_info.index(p)]['name'] = Laptop.find(p.laptop_id).name
- 	    @products[@products_info.index(p)]['price'] = Laptop.find(p.laptop_id).price
- 	    @products[@products_info.index(p)]['quantity'] = p.quantity 
- 	  } 
+    if session[:user_id]!=''
+      @user_id = session[:user_id]	
+      find_products(@user_id)
   	else
   	  redirect_to user_login_path
   	end	
