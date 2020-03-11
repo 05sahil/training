16e2a88d8ca6fe46f51817b6185c12aa60eb48af
05sahil <sahil.jha@berylsystems.com>
2020-03-11 16:34:38 +0530

    add show action logic in order to  view current user products

--- laptop_store/app/controllers/carts_controller.rb
+++ laptop_store/app/controllers/carts_controller.rb
@@ -1,4 +1,16 @@
 class CartsController < ApplicationController
   def show
+    if session[:user_id]!=''	
+      @user_id = session[:user_id]
+  	  @products_info = User.find(@user_id).orders.find_by("payment_status != 'received'").order_products
+ 	  @products = Array.new(@products_info.length){Hash.new}  #@product_info.length
+ 	  @products_info.each{|p| 
+ 	    @products[@products_info.index(p)]['name'] = Laptop.find(p.laptop_id).name
+ 	    @products[@products_info.index(p)]['price'] = Laptop.find(p.laptop_id).price
+ 	    @products[@products_info.index(p)]['quantity'] = p.quantity 
+ 	  } 
+  	else
+  	  redirect_to user_login_path
+  	end	
   end
 end
