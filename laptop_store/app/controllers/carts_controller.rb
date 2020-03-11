cb08c3ba2f2cb1a56745745437754a2cd75a1e65
05sahil <sahil.jha@berylsystems.com>
2020-03-11 16:58:19 +0530

    add destroy action logic to remove cart item

--- laptop_store/app/controllers/carts_controller.rb
+++ laptop_store/app/controllers/carts_controller.rb
@@ -13,4 +13,11 @@ class CartsController < ApplicationController
   	  redirect_to user_login_path
   	end	
   end
+  def destroy
+    @name = params[:product_name]
+    @user_id = session[:user_id]
+    @product_id = Laptop.find_by(name: @name).id
+    User.find(@user_id).orders.find_by("payment_status != 'received'").order_products.find_by(laptop_id: @product_id).delete
+    redirect_to user_cart_path
+  end
 end
