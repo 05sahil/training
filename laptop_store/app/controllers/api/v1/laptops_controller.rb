0082b149d3c832837362fbc4e9c45eab49046c45
05sahil <sahil.jha@berylsystems.com>
2020-03-12 19:14:18 +0530

    create laptop update and destroy action

--- laptop_store/app/controllers/api/v1/laptops_controller.rb
+++ laptop_store/app/controllers/api/v1/laptops_controller.rb
@@ -8,8 +8,34 @@ module Api
 
       def show
         laptop = Laptop.find(params[:id])
-        render json: {data: laptop, status: 200, message: 'loaded laptop'}
+        render json: {data: laptop, status: 'SUCCESS', message: 'loaded laptop'}
       end
+      def update
+        laptop = Laptop.find(params[:id])
+        if laptop.update(laptop_params)
+          render json: {data: laptop, status: 'SUCCESS', message: 'laptop updated'} 
+        else 
+          render json: {data: laptop.errors, status: 'FAILURE', message: 'laptop not updated'} 
+        end
+      end
+      def destroy
+        laptop = Laptop.find(params[:id])
+        spec = laptop.laptop_spec
+        if spec
+          spec.delete
+        end
+        if laptop.delete
+          render json: {status: 'SUCCESS', message: 'laptop deleted'} 
+        else 
+          render json: {data: laptop, status: 'FAILURE', message: 'laptop not deleted'} 
+        end
+      end
+
+      private
+      def laptop_params
+        params.require(:laptop).permit(:brand,:name,:price,:quantity)
+      end
+
     end
   end
 end
