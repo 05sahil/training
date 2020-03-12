560dbedb631b2a9982c73d44a8009375d4a7cbe8
05sahil <sahil.jha@berylsystems.com>
2020-03-12 11:09:58 +0530

    api - add create action

--- laptop_store/app/controllers/api/v1/users_controller.rb
+++ laptop_store/app/controllers/api/v1/users_controller.rb
@@ -3,8 +3,23 @@ module Api
   	class UsersController < ApplicationController
   	  def index
   	  	users = User.all
-  	  	render json: {status: 'loaded all users', message: 'all users', data: users}, status: :ok
+  	  	render json: {status: 'SUCCESS', message: 'loaded all users', data: users}, status: :ok
   	  end  
+
+  	  def create
+  	  	user = User.new(user_params)
+  	  	if user.save
+  	  	  render json: {status: 'SUCCESS', message: 'user saved', data: user}, status: :ok
+  	  	else
+  	  	  render json: {status: 'FAILURE', message: 'user not saved', data: user.errors}, status: 422
+  	  	end
+  	  end
+
+  	  private 
+
+  	  def user_params
+        	params.require(:user).permit(:name,:username,:password,:email,:phoneno,:address,:password_confirmation)
+      end
   	end
   end
 end
