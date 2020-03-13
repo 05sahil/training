df344b521c3632cfc9f3dec77158d19355587917
05sahil <sahil.jha@berylsystems.com>
2020-03-13 17:38:08 +0530

    add login action in sessions controller and define its route

--- laptop_store/app/controllers/api/v1/main_controller.rb
+++ laptop_store/app/controllers/api/v1/main_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class MainController < ApplicationController::API
+    class MainController < ActionController::API
 	end
   end
 end
--- laptop_store/config/routes.rb
+++ laptop_store/config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
      namespace 'v1' do 
        resources :users
        resources :laptops
+       post "user/login", to: 'sessions#login'
     end
    end 
 =begin
