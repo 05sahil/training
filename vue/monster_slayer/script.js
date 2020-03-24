9a4ce1c67afed8abc5bc827bf435dfd220abf4f9
05sahil <sahil.jha@berylsystems.com>
2020-03-24 19:45:43 +0530

    update attack method

--- vue/monster_slayer/script.js
+++ vue/monster_slayer/script.js
@@ -11,7 +11,10 @@ new Vue({
   },
   methods: {
   	attack: function() {
-
+  	  this.y_attack = Math.floor(Math.random()*10)+5;
+  	  this.m_attack = Math.floor(Math.random()*10)+5;
+  	  this.you -= this.y_attack;
+  	  this.monster -= this.m_attack;
   	}
   }
 })
