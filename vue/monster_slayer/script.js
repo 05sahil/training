71be110498b549fe1eacfeb797d0bfe356526253
05sahil <sahil.jha@berylsystems.com>
2020-03-24 19:32:55 +0530

    add attack method and game values

--- vue/monster_slayer/script.js
+++ vue/monster_slayer/script.js
@@ -3,6 +3,15 @@ console.log('monster')
 new Vue({
   el: '#app',
   data: {
-  	con_value: true
+  	con_value: true,
+  	you: 100,
+  	monster: 100,
+  	y_attack: 0,
+  	m_attack: 0
+  },
+  methods: {
+  	attack: function() {
+
+  	}
   }
 })
