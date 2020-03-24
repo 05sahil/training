d5380d009154ad223e9f449574b51c78becda874
05sahil <sahil.jha@berylsystems.com>
2020-03-24 22:33:13 +0530

    add battle display functionality

--- vue/monster_slayer/script.js
+++ vue/monster_slayer/script.js
@@ -1,5 +1,9 @@
 console.log('monster')
 
+var com = {
+  template: '<h1>I ama a template</h1>'	
+}
+
 new Vue({
   el: '#app',
   data: {
@@ -7,7 +11,11 @@ new Vue({
   	you: 100,
   	monster: 100,
   	y_attack: 0,
-  	m_attack: 0
+  	m_attack: 0,
+  	battles: []
+  },
+  components: {
+  	mycomp: com,
   },
   methods: {
   	attack: function(sa=false) {
@@ -18,6 +26,13 @@ new Vue({
   	  this.you -= this.y_attack;
   	  this.monster -= this.m_attack;
   	  console.log('attack')
+  	  var battle = {
+  	  	you: 'Player hits monster for '+ this.m_attack,
+  	    monster: 'Monster hits player for ' + this.y_attack
+  	  }
+  	  this.battles.push(battle)
+  	  // var html = "<p>Player hits monster for {{ this.m_attack }}</p><p>Monster hits player for {{ this.y_attack }}</p>"
+  	  // document.querySelector('#attack_display').insertAdjacentHTML('beforeend','<mycomp></mycomp>')
   	},
   	heal: function(){
   	var heal_val = 10;
@@ -25,7 +40,14 @@ new Vue({
   	this.y_attack = Math.floor(Math.random()*10)+5;
   	this.you -= this.y_attack;
   	console.log('heal')
+  	var heal = {
+  	  you: 'Player Heals for ' + heal_val,
+  	  monster: 'Monster hits player for ' + this.y_attack
+  	}
+  	this.battles.push(heal)
   	},
+
   },
 
 })
+
