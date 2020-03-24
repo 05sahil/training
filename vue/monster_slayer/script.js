05b8c97fbd2d0f5f64390fbf3fc527377f6d0ad9
05sahil <sahil.jha@berylsystems.com>
2020-03-25 01:22:32 +0530

    add win logic

--- vue/monster_slayer/script.js
+++ vue/monster_slayer/script.js
@@ -1,9 +1,5 @@
 console.log('monster')
 
-var com = {
-  template: '<h1>I ama a template</h1>'	
-}
-
 new Vue({
   el: '#app',
   data: {
@@ -12,14 +8,35 @@ new Vue({
   	monster: 100,
   	y_attack: 0,
   	m_attack: 0,
-  	battles: []
+  	battles: [],
+  	player_img: './assests/player_img.png',
+  	monster_img: './assests/dragon_img.png',
+  	img_val: false
   },
-  components: {
-  	mycomp: com,
+  watch: {
+  	you: function(){
+  	  if(this.you<=0)
+  	  {
+  	  	alert('You Loose!!')
+  	  	console.log('you win')
+  	  } 
+  	  if(this.monster<=0)
+  	  {
+  	  	alert('You Win!!')
+  	  	console.log('you loose')
+  	  }  
+  	}
   },
   methods: {
+  	// win: function(){
+  	//   console.log(this.you, typeof(this.you))
+  	  
+  	// },
+
   	attack: function(sa=false) {
+  	  this.img_val = true
   	  var random_val = 0
+  	  //condition for special attack
   	  sa?random_val=10:random_val=5;
   	  this.y_attack = Math.floor(Math.random()*10)+5;
   	  this.m_attack = Math.floor(Math.random()*10)+random_val;
@@ -31,20 +48,22 @@ new Vue({
   	    monster: 'Monster hits player for ' + this.y_attack
   	  }
   	  this.battles.push(battle)
-  	  // var html = "<p>Player hits monster for {{ this.m_attack }}</p><p>Monster hits player for {{ this.y_attack }}</p>"
-  	  // document.querySelector('#attack_display').insertAdjacentHTML('beforeend','<mycomp></mycomp>')
   	},
+
   	heal: function(){
-  	var heal_val = 10;
-  	this.you += heal_val;
-  	this.y_attack = Math.floor(Math.random()*10)+5;
-  	this.you -= this.y_attack;
-  	console.log('heal')
-  	var heal = {
-  	  you: 'Player Heals for ' + heal_val,
-  	  monster: 'Monster hits player for ' + this.y_attack
-  	}
-  	this.battles.push(heal)
+  	  if(this.you<=90)
+  	  {
+  	  	var heal_val = 10;
+  	    this.you += heal_val;
+  	    this.y_attack = Math.floor(Math.random()*10)+5;
+  	    this.you -= this.y_attack;
+  	    console.log('heal')
+  	    var heal = {
+  	      you: 'Player Heals for ' + heal_val,
+  	      monster: 'Monster hits player for ' + this.y_attack
+  	    }
+  	    this.battles.push(heal)
+  	  }
   	},
 
   },
