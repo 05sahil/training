424a8dce8494020339ad2fa453a2512d2a714e60
05sahil <sahil.jha@berylsystems.com>
2020-03-26 18:42:22 +0530

    add local component - cli template

---
+++ vue/vue_practice/src/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <server_status_comp v-for="server in 5"></server_status_comp>
+  </div>
+</template>
+
+<script>
+  import comp from './Status.vue' 
+  export default{
+    components: {
+      server_status_comp: comp
+    }
+  } 
+</script>
+
+<style>
+
+</style>
--- vue/vue_practice/src/main.js
+++ vue/vue_practice/src/main.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import status_obj from './Status.vue'
+import status_obj from './Home.vue'
 
-Vue.component('status_comp',status_obj)
+// Vue.component('status_comp',status_obj)
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(status_obj)
 })
 
-new Vue({
-  el: '#app2'
-})
+// new Vue({
+//   el: '#app2'
+// })
