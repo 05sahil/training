7b1813102d41877e303af390be8ca6175f00ab44
05sahil <sahil.jha@berylsystems.com>
2020-04-06 10:22:15 +0530

    add components author and quote

---
+++ vue/vue_practice/src/Author.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h4>Di caprio</h4>
+  </div>
+</template>
+
+<script>
+  export default{
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue/vue_practice/src/Quote.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h3>Learn every moment {{ this.x }}</h3>
+  </div>
+</template>
+
+<script>
+
+  export default{
+    data () {
+      return {
+        x: '23'
+      }
+    }
+  }
+
+</script>
+
+<style>
+
+</style>
