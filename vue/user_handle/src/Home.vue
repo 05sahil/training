776a63d331c04d0c0bb115074ad7167316dd4fdb
05sahil <sahil.jha@berylsystems.com>
2020-04-09 11:27:08 +0530

    imperative navigation

--- vue/user_handle/src/Home.vue
+++ vue/user_handle/src/Home.vue
@@ -5,13 +5,18 @@
       <router-link to="/user/1" tag="li">User 1</router-link>
       <router-link to="/user/2" tag="li">User 2</router-link>
       <router-link to="/user/3" tag="li">User 3</router-link>
+      <li @click="getUser">User 4</li>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    getUser(){
+      this.$router.push('/user/4')
+    }
+  }
 }
 </script>
 
