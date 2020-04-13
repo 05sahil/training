db6474a6cb4bd2eeec5ca438cb6d3ccb61457c85
05sahil <sahil.jha@berylsystems.com>
2020-04-14 02:36:05 +0530

    add quote component

---
+++ vue/aws_quotes/src/Quote.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="quote">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    quoteData: Object
+  },
+}
+</script>
+
+<style scoped>
+  #quote {
+    padding: 20px;
+    width: 15%;
+    border: 2px solid dodgerblue;
+  }
+
+</style>
--- vue/aws_quotes/src/main.js
+++ vue/aws_quotes/src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import CreateQuote from './CreateQuote.vue'
 import ProgBar from './ProgBar.vue'
+import Quote from './Quote.vue'
 
 Vue.component('createQuote', CreateQuote)
 Vue.component('progBar', ProgBar)
+Vue.component('vQuote', Quote)
 
 new Vue({
   el: '#app',
